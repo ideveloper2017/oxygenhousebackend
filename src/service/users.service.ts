449f9d43a1c7090ddf5122c46bb85a3c9df4f39d
Bahrom Parpiyev <bahrom1982@gmail.com>
2023-08-17 10:24:54 +0500

    commit

--- src/service/users.service.ts
+++ src/service/users.service.ts
@@ -2,14 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 import { Users } from 'src/entity/users.entity';
 import { DataSource } from 'typeorm';
+import {CreateUserDto} from "../dtos/user-dto/create-user.dto";
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly dataSource: DataSource,) {}
+  constructor(private readonly dataSource: DataSource) {}
 
   async getUsers() {
     const users = await this.dataSource.manager.find(Users);
     return { message: 'All users', data: users };
+  }
+
+  public async signin(userDto:CreateUserDto){
+    await this.dataSource.manager.getRepository(Users).find({where:{}})
   }
 
   
