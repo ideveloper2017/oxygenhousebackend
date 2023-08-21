f744541d344f756c2a263938e966b22bbb7a7dc5
iDeveloper <bahrom1982@gmail.com>
2023-08-21 13:24:00 +0500

    Commit

--- src/dtos/town-dto/create-town.dto.ts
+++ src/dtos/town-dto/create-town.dto.ts
@@ -1,8 +1,16 @@
+import {ApiProperty} from "@nestjs/swagger";
+
 export class CreateTownDto {
+  @ApiProperty()
   name: string;
+  @ApiProperty()
   region_id: number;
+  @ApiProperty()
   district_id: number;
+  @ApiProperty()
   address?: string;
+  @ApiProperty()
   contact_number?: string;
+  @ApiProperty()
   logo?: string;
 }
