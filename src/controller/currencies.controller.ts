efb1a347b11b55757dbd7f4fa222b86bfc2de251
iDeveloper <bahrom1982@gmail.com>
2023-08-21 13:56:54 +0500

    Commit

--- src/controller/currencies.controller.ts
+++ src/controller/currencies.controller.ts
@@ -4,19 +4,17 @@ import { CurrenciesService } from 'src/service/currencies.service';
 
 @Controller('currency')
 export class CurrenciesController {
+  constructor(private readonly currancyService: CurrenciesService) {}
 
-    constructor( private readonly currancyService: CurrenciesService){}
+  @ApiOperation({ summary: "Pul birliklari ro'yxatini olish" })
+  @Get('/list')
+  viewCurrancies() {
+    return this.currancyService.getCurrencies();
+  }
 
-    @ApiOperation({summary: "Pul birliklari ro'yxatini olish"})
-    @Get('/list')
-    viewCurrancies() {
-        return this.currancyService.getCurrencies()
-    }
-
-    @ApiOperation({summary: "Pul birligini belgilash"})
-    @Patch('/option')
-    selectCurrency(@Body() currency: number[] ) {
-        return this.currancyService.selectCurrency(currency)
-    }
-    
+  @ApiOperation({ summary: 'Pul birligini belgilash' })
+  @Patch('/option')
+  selectCurrency(@Body() currency: number[]) {
+    return this.currancyService.selectCurrency(currency);
+  }
 }
