1ea6e4d41decde1d69b8edd588f10b1aaf21dd5a
iDeveloper <bahrom1982@gmail.com>
2023-08-21 13:57:42 +0500

    Commit

--- src/service/currencies.service.ts
+++ src/service/currencies.service.ts
@@ -5,19 +5,20 @@ import { Repository } from 'typeorm';
 
 @Injectable()
 export class CurrenciesService {
+  constructor(
+    @InjectRepository(Currencies)
+    private readonly currencyRepo: Repository<Currencies>,
+  ) {}
 
-    constructor(@InjectRepository(Currencies) private readonly currencyRepo: Repository<Currencies>) {}
+  async getCurrencies() {
+    const currencies = await this.currencyRepo.find();
+    return { status: 200, data: currencies, message: 'Success' };
+  }
 
-    async getCurrencies() {
-        const currencies = await this.currencyRepo.find()
-        return {status: 200, data: currencies, message: 'Success'} 
+  async selectCurrency(arrayOfId: number[]) {
+    for (const id of arrayOfId) {
+      await this.currencyRepo.update({ id: id }, { is_selected: true });
     }
-
-    async selectCurrency(arrayOfId: number[] ) {
-        for (let id of arrayOfId) {
-           await this.currencyRepo.update({id: id},{is_selected: true}) 
-        }
-        return {status: 200, data:[], message: 'Success'}
-    }
-
+    return { status: 200, data: [], message: 'Success' };
+  }
 }
