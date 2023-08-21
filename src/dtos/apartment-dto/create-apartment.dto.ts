b258b253801389787e4090fc9a782cd278f3b2b2
iDeveloper <bahrom1982@gmail.com>
2023-08-21 13:55:21 +0500

    Commit

--- src/dtos/apartment-dto/create-apartment.dto.ts
+++ src/dtos/apartment-dto/create-apartment.dto.ts
@@ -1,9 +1,16 @@
 import { Buildings } from 'src/entity/buildings.entity';
+import {ApiProperty} from "@nestjs/swagger";
 export class CreateApartmentDto {
+  @ApiProperty()
   building_id: Buildings;
+  @ApiProperty()
   entrance: number;
+  @ApiProperty()
   floor: number;
+  @ApiProperty()
   room_number: number;
+  @ApiProperty()
   cells: number;
+  @ApiProperty()
   room_space: number;
 }
