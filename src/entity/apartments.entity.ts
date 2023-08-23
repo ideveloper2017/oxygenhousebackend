3d37dc5333e3d49cbc9c9505dd4f3416b9f5b403
Azimkhoja <ahmadkhonovazimkhoja@.com>
2023-08-23 19:04:37 +0500

    added: Createcurrency

--- src/entity/apartments.entity.ts
+++ src/entity/apartments.entity.ts
@@ -3,9 +3,7 @@ import Model from './model.entity';
 import {Buildings} from './buildings.entity';
 import {Price} from './price.entity';
 import {Sale_details} from './sale_details.entity';
-import {ApiTags} from '@nestjs/swagger';
 
-@ApiTags('Apartments')
 @Entity('Apartments')
 export class Apartments extends Model {
     @ManyToOne((type) => Buildings, (building) => building.apartments)
